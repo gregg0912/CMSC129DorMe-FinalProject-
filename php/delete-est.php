b1c3ea59bd648fb1929b0ef7123be6ee9061dcdd
linseeh <lllegada@gmail.com>
2017-02-26 23:40:36 +0800

    fixed minor errors from delete-est

--- php/delete-est.php
+++ php/delete-est.php
@@ -2,17 +2,26 @@
 	session_start();
 	require("function.php");
 	$dbconn = dbconn();
-
+	$ownerID = $_SESSION['userID'];
 		$del = $_POST['row'];
 		$query = "DELETE FROM dorm WHERE DormId = '$del'";
 		$result = mysqli_query($dbconn, $query);
 
-		$query = "SELECT * FROM dorm WHERE OwnerId = '$_SESSION[userID]'";
+		$query = "SELECT dorm.DormId, dorm.DormName, dorm.HousingType, owner.Name, dorm.Location, dorm.thumbnailpic, CONCAT(address.StreetName,', ',address.Barangay) FROM dorm, address, owner WHERE dorm.OwnerId = $ownerID AND owner.OwnerId= $ownerID AND dorm.AddressId = address.AddressId";
 		$result = mysqli_query($dbconn, $query);
 
 ?>
-	<div id='manage-est'>
-		<?php
-			renderlist($result,"","");
-		?>
-	</div>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DorMe | Manage</title>
+</head>
+<body>
+<nav id="manage-est">
+	<a href="add-est.php">Add Establishment</a
+</nav>
+	<?php
+		renderlist($result,"","");
+	?>
+</body>
+</html>
