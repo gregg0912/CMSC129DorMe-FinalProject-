bd8ad94ac1214a0a18599f6659d9a1c5f5725679
gregg <gmicay123@gmail.com>
2017-03-01 17:06:59 +0800

    editing delete mech

--- php/delete-est.php
+++ php/delete-est.php
@@ -4,24 +4,26 @@
 	$dbconn = dbconn();
 	$ownerID = $_SESSION['userID'];
 		$del = $_POST['row'];
-		$query = "DELETE FROM dorm WHERE DormId = '$del'";
+		$query = array("DELETE FROM dorm_number WHERE DormId = '$del'",
+						"DELETE FROM dorm_pictures WHERE DormId = '$del'",
+						"DELETE FROM dorm_room WHERE DormId = '$del'",
+						"DELETE FROM facility_dorm WHERE DormId = '$del'",
+						"DELETE FROM dorm WHERE DormId = '$del'");
+
+		foreach ($queries as $query) {
+			$result = mysqli_query($dbconn, $query);
+			if(!$result){
+			?>
+				<script type='text/javascript'>alert('Something went wrong. Please try again.')</script>
+			<?php
+			}
+		}
+
+		$query = "SELECT dorm.DormId, dorm.DormName,CONCAT(address.StreetName,', ',address.Barangay), owner.Name, dorm.HousingType, dorm.thumbnailpic
+				FROM dorm, address, owner
+				WHERE dorm.OwnerId = $ownerID AND owner.OwnerId= $ownerID AND dorm.AddressId = address.AddressId";
 		$result = mysqli_query($dbconn, $query);
 
-		$query = "SELECT dorm.DormId, dorm.DormName, dorm.HousingType, owner.Name, dorm.Location, dorm.thumbnailpic, CONCAT(address.StreetName,', ',address.Barangay) FROM dorm, address, owner WHERE dorm.OwnerId = $ownerID AND owner.OwnerId= $ownerID AND dorm.AddressId = address.AddressId";
-		$result = mysqli_query($dbconn, $query);
 
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>DorMe | Manage</title>
-</head>
-<body>
-<nav id="manage-est">
-	<a href="add-est.php">Add Establishment</a
-</nav>
-	<?php
 		renderlist($result,"","","viewOwner");
-	?>
-</body>
-</html>
+?>
