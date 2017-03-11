bc68a5e8846b2d920d9eb95cb7ca34f625c59d15
shebna12 <shebna12@gmail.com>
2017-03-11 14:34:16 +0800

    edit thumb

--- php/add-est.php
+++ php/add-est.php
@@ -3,7 +3,6 @@
 	require("function.php");
 	$dbconn = dbconn();
 	ownerRedirect();
-	$ownerId = $_SESSION['userID'];
 	$estName = "";
 	$streetName = "";
 	$barangayName = "";
@@ -13,19 +12,19 @@
 	$hType = "";
 	$facilityList = "";
 	$addOn = "";
-	$addItem = "";
-	$addPrice = "";
 	$maxNum = "";
 	$typeOfPayment = "";
 	$price = "";
 	$errorMsg = "";
-	$successMsg = "";
 	if(isset($_POST['submit'])){
 		$estName = $_POST['estName'];
 		$streetName = $_POST['streetName'];
 		$barangayName = $_POST['barangayName'];
 		$cellnum = $_POST['cellnum'];
 		$telnum = $_POST['telnum'];
+		$price = $_POST['price'];
+		$maxNum = $_POST['maxNum'];
+		$price = $_POST['price'];
 		if(!empty($_POST['loc'])){
 			$loc = $_POST['loc'];
 		}
@@ -41,36 +40,7 @@
 		if(!empty($_POST['typeOfPayment'])){
 			$typeOfPayment = $_POST['typeOfPayment'];
 		}
-		if(!empty($_POST['maxNum'])){
-			$maxNum = $_POST['maxNum'];
-		}
-		if(!empty($_POST['price'])){
-			$price = $_POST['price'];
-		}
-		if(!empty($_POST['add-item'])){
-			$addItem = $_POST['add-item'];
-		}
-		if(!empty($_POST['add-price'])){
-			$addPrice = $_POST['add-price'];
-		}
-		list($errorMsg, $successMsg, $errors) = addEst($dbconn, $ownerId, $estName, $streetName, $barangayName, $cellnum, $telnum, $loc, $hType, $facilityList, $addOn, $addItem, $addPrice, $typeOfPayment, $maxNum, $price);
-		if($errors == 0){
-			$estName = "";
-			$streetName = "";
-			$barangayName = "";
-			$cellnum = "";
-			$telnum = "";
-			$loc = "";
-			$hType = "";
-			$facilityList = "";
-			$addOn = "";
-			$addItem = "";
-			$addPrice = "";
-			$maxNum = "";
-			$typeOfPayment = "";
-			$price = "";
-			$errorMsg = "";
-		}
+		list($errorMsg, $successMsg) = addEst($estName, $streetName, $barangayName, $cellnum, $telnum, $loc, $hType, $facilityList, $addOn, $typeOfPayment, $maxNum, $price);
 	}
 ?>
 <!DOCTYPE html>
@@ -92,21 +62,12 @@
 <div>
 	<form method="POST" action="" id="add-form">
 		<div class="msg">
-			<p>
-			<?php
-			if(!empty($errorMsg))
-				echo "$errorMsg";
-			else
-				echo "$successMsg";
-			?>
-			</p>
+			<p><?=$errorMsg?></p>
 		<fieldset>
 			<legend>Establishment Information</legend>
 			<input type="text" name="estName" required="required" placeholder="Establishment Name" value="<?=$estName?>" />
-			<input type="text" name="streetName" required="required" placeholder="Street Name" value="<?=$streetName?>" list="street" />
-			<?=addressDataList("street");?>
-			<input type="text" name="barangayName" required="required" placeholder="Barangay Name" value="<?=$barangayName?>" list="barangay" />
-			<?=addressDataList("barangay")?>
+			<input type="text" name="streetName" required="required" placeholder="Street Name" value="<?=$streetName?>" />
+			<input type="text" name="barangayName" required="required" placeholder="Barangay Name" value="<?=$barangayName?>" />
 		</fieldset>
 		<fieldset>
 			<legend>Contact Information</legend>
@@ -127,34 +88,27 @@
 		</fieldset>
 		<fieldset id="TextBoxesGroup" name="TextBoxesGroup">
 			<legend>Facilities</legend>
-			<div id="checkboxEstGroup">
 			<?php
 			checkboxEst();
 			?>
-			</div>
 			<input type="button" class="add-btn" id="add-btn" name="add-btn" value="Add Option" />
 		</fieldset>
-		<fieldset id="RoomsGroup" name="RoomsGroup">
+		<fieldset>
 			<legend>Rooms</legend>
-			<div id="roomDiv1">
-				<label>Maximum number of residents: <input type="number" name="maxNum[]" min="1" value="1" /></label>
-				<label>
-					Type of Payment:
-					<select name="typeOfPayment[]" id="typeOfPayment">
-						<option value="by_room">Per Room</option>
-						<option value="by_person">Per Person</option>
-					</select>
-				</label>
-				<label>Price: <input type="number" name="price[]" min="500" value="500" /></label>
-			</div>
+			<label>Maximum number of residents: <input type="number" name="maxNum" min="1" value="1" /></label>
+			<label>
+				Type of Payment:
+				<select name="typeOfPayment" id="typeOfPayment">
+					<option value="by_room">Per Room</option>
+					<option value="by_person">Per person</option>
+				</select>
+			</label>
+			<label>Price: <input type="number" name="price" min="500" value="500" /></label>
 			<input type="button" class="add-btn" id="add-btn2" name="add-btn2" value="Add Option" />
 		</fieldset>
-		<fieldset id="AddOnGroup" name="AddOnGroup">
+		<fieldset>
 			<legend>Add-On</legend>
-			<div id="checkboxAddGroup">
-				<?=checkboxAdd();?>
-			</div>
-			<input type="button" name="add-btn" id="add-btn3" name="add-btn3" value="Add Option" />
+			<?=checkboxAdd();?>
 		</fieldset>
 		<input type="submit" name="submit" value="Submit" />
 	</form>
