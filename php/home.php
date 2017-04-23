c1b513ce990a3bf8ba10d12e03597d5fc8bb0326
Cyra Dawn <cyradawn@gmail.com>
2017-04-23 18:31:48 +0800

    Add files via upload

--- php/home.php
+++ php/home.php
@@ -7,34 +7,38 @@
 <html>
 <head>
 	<title>DorMe | Home</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../bootstrap-3.3.7/dist/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="../bootstrap-3.3.7/dist/css/bootstrap.css" rel="stylesheet" />
+	<link href="../css/style.css" rel="stylesheet" type="text/css" />
+	<link href="../css/home.css" rel="stylesheet" type="text/css" />
+	<script src="../bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
+	<script src="../js/script.js"></script>
+	<script src="../js/js.js"></script>
+	<script src="../js/jquery.js"></script>
 </head>
 <body>
 	<?php
 	headerRender();
 	?>
-	<h1>DorMe.</h1>
-	<h2>your dorm. my dorm. our dorm.</h2>
-	<p> Looking for convenience? Look no further. Dorme is here for your new place to dwell!<br />
-		Scroll through featured dormitories and apartments on our home page and <br />
-		have an easy glimpse into finding your perfect second home!<br />
-		Sit back and pick your like.
-	</p>
-	<section id="establishments">
-		<h2>Featured Establishments</h2>
-		<?php
-			$query = homequeryConstruct();
-			$result = mysqli_query($dbconn, $query);
-			renderlist($result, "", "", "home");
-		?>
-		<a href="#header">Back to Top</a>
-	</section>
-	<section id='contacthome'>
-		<h2>Contact Us</h2>
-		<p> Questions? Feedback? Suggestions? <br> We'd love to hear from you!<br>
-			Send us an email at <strong><a href="javascript:void(0)">support@dorme.com</a></strong> and we'll get back to you as soon as possible.<br>
-		</p>
-	</section>
+	<div class='body-content col-md-12 col-sm-12'>
+		<div id="establishments">
+			<h2>Featured Establishments</h2>
+			<?php
+				$query = homequeryConstruct();
+				$result = mysqli_query($dbconn, $query);
+				renderlist($result, "", "", "home");
+			?>
+			<a href="#header">Back to Top</a>
+		</div>
+		<div id='contacthome'>
+			<h2>Contact Us</h2>
+			<p> Questions? Feedback? Suggestions? <br> We'd love to hear from you!<br>
+				Send us an email at <strong><a href="javascript:void(0)">support@dorme.com</a></strong> and we'll get back to you as soon as possible.<br>
+			</p>
+		</div>
+	</div>
 	<footer>
 		<p>&copy; Dorme 2016 | A.Y. 2016-2017 CMSC 127: Fabilloren, Icay, Legada, Montano</p>
 	</footer>
