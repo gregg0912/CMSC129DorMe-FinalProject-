28ac481576f20795c62efa386e573eba18262c53
gregg <gmicay123@gmail.com>
2017-05-06 16:02:47 +0800

    editing votes

--- 129crashcourse/resources/views/dorm/vote.blade.php
+++ 129crashcourse/resources/views/dorm/vote.blade.php
@@ -8,7 +8,7 @@
 			<form role="form">
 				<h1>Establishments</h1>
 				@forelse($dorms as $dorm)
-					<div class="col-md-6 radio">
+					<div class="radio">
 						<label>
 							<input type="radio" name="establishment" id="{{ $dorm->id }}" value="{{ $dorm->id }}" /> {{ $dorm->dormName }}
 						</label>
@@ -16,6 +16,12 @@
 				@empty
 					<h4>No establishments  were found!</h4>
 				@endforelse
+
+				@if(!$dorms->isEmpty())
+					<div class="form-group">
+						<input type="submit" name="submit" class="btn btn-success" value="Submit" />
+					</div>
+				@endif
 			</form>
 		</div>
 	</div>
