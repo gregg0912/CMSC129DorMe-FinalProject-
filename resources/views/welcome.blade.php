c1f2e0da1da81a26725deb9247b83243d3a4d3a5
Cyra Dawn <cyradawn@gmail.com>
2017-05-20 21:34:02 +0800

    commit changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+<link media="all" type="text/css" rel="stylesheet" href="{{ URL::asset('../css/style.css') }}" />
+<link media="all" type="text/css" rel="stylesheet" href="{{ URL::asset('../css/home.css') }}" />
+<script rel="stylesheet" src="{{ URL::asset('../js/script1.js') }}"></script>
+<script rel="stylesheet" src="{{ URL::asset('../js/home.js') }}"></script>
+
+
 @section('content')
 <div class="flex-center position-ref full-height">
     <div class="content">
@@ -28,7 +34,8 @@
                     </div>
                 @endforelse
             </div>
-            <div id="contacthome">
+        </div>
+        <div id="contacthome">
                 <h2>Contact Us</h2>
                 <p> Questions? Feedback? Suggestions? <br> We'd love to hear from you!<br>
                     Send us an email at <strong><a href="javascript:void(0)">support@dorme.com</a></strong> and we'll get back to you as soon as possible.<br>
