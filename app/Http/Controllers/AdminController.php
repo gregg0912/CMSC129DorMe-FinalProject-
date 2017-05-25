0c139dc81255013b9dc027bab9d64223a4c2f624
shebna12 <shebna12@gmail.com>
2017-05-26 02:24:15 +0800

    Approve Requests done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,10 @@ use App\RequestDorm;
 use App\RequestFacility;
 use App\Dorm;
 use App\Facility;
+use App\RequestAddon;
+use App\Addon;
+use App\RequestRoom;
+use App\Room;
 use DB;
 
 class AdminController extends Controller
@@ -37,32 +41,75 @@ class AdminController extends Controller
     {
     	$rdorm = RequestDorm::find($dorm_id);
     	$this->addToDorm($rdorm);
-    	$this->getFacilities($dorm_id);
+    	$this->getSendFacilities($dorm_id);
+    	$this->getSendAddon($dorm_id);
+    	 $this->getSendRoom($dorm_id);
+    	 $this->deleteFromRoom($dorm_id);
+    	$this->deleteFromAddon($dorm_id);
+    	$this->deleteFromFacilities($dorm_id);
+    	$this->deleteFromDorm($dorm_id);
 
-
-    	
-    	// dd($dorm_id);
-    	//add stuff to real stuff
-    	
-    	//copy query from requestdorm
-    	//delete record from requestdorm
-    	//query to dorms
+ 
     }
-    public function getFacilities($dorm_id)
+      public function deleteFromDorm($dorm_id)
+    {
+    	//dd($rfac);
+    	// $del = RequestDorm::find(2);
+    	return RequestDorm::where('id', $dorm_id->id)->delete();
+    	// return $del;
+    }
+     public function deleteFromRoom($dorm_id)
+    {
+    	//dd($rfac);
+    	return RequestRoom::where('request_id', $dorm_id->id)->delete();
+    }
+    public function deleteFromAddon($dorm_id)
+    {
+    	//dd($rfac);
+    	return RequestAddon::where('request_id', $dorm_id->id)->delete();
+
+    }
+    public function deleteFromFacilities($dorm_id)
+    {
+    	//dd($rfac);
+    	return RequestFacility::where('request_id', $dorm_id->id)->delete();
+
+    }
+
+
+     public function getSendRoom($dorm_id)
+    {
+    	$rRoom = RequestRoom::find($dorm_id);
+    	//dd($rfac);
+    	return Room::create([
+            'dorm_id' => $rRoom['request_id'],
+            'maxNoOfResidents' => $rRoom['maxNoOfResidents'],
+            'typeOfPayment' => $rRoom['typeOfPayment'],
+            'price' => $rRoom['price'],
+        ]);
+    }
+    public function getSendAddon($dorm_id)
+    {
+    	$rAddon = RequestAddon::find($dorm_id);
+    	//dd($rfac);
+    	return Addon::create([
+            'add_id' => $rAddon['request_id'],
+            'add_item' => $rAddon['add_item'],
+            'add_price' => $rAddon['add_price'],
+        ]);
+    }
+    public function getSendFacilities($dorm_id)
     {
     	$rfac = RequestFacility::find($dorm_id);
     	//dd($rfac);
-    	$this->sendFacilities($rfac);
-
-    }
-    public function sendFacilities($rfac)
-    {
     	return Facility::create([
             'dorm_id' => $rfac['request_id'],
             'facility_name' => $rfac['facility_name'],
             
         ]);
+
     }
+   
     public function addToDorm($rdorm)
     {
     	return Dorm::create([
