a39639d5bd646bcb4f67c5879e4de83414ca1e0d
Cyra Dawn <cyradawn@gmail.com>
2017-05-27 14:53:17 +0800

    retrieve vote.blade.php @ gregg

--- resources/views/dorm/index.blade.php
+++ resources/views/dorm/index.blade.php
@@ -111,7 +111,7 @@
 	</div>
 
 </div>
-<footer>
+<footer class="navbar navbar-fixed-bottom">
     <p>&copy; Dorme 2016 | A.Y. 2016-2017 CMSC 127: Fabilloren, Icay, Legada, Montano</p>
 </footer>
 @endsection
